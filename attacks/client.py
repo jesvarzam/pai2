be6174ab0bccfe2065d4944974042a7eb5b934d0
Daniel Muñoz Heredia <dsgpolaris55@gmail.com>
2022-03-23 18:55:46 +0100

    Minor changes

--- attacks/client.py
+++ attacks/client.py
@@ -13,22 +13,30 @@ def generate_and_send_key(client):
 	client.send(str.encode(key))
 	return str.encode(key)
 
+def calculate_hmac(key, message, alg_cript):
+	if alg_cript == "1":
+		return hmac.new(key, str.encode(message), hashlib.sha256).hexdigest()
+	elif alg_cript == "2":
+		return hmac.new(key, str.encode(message), hashlib.sha512).hexdigest()
+	elif alg_cript == "3":
+		return hmac.new(key, str.encode(message), hashlib.sha3_256).hexdigest()
+	elif alg_cript == "4":
+		return hmac.new(key, str.encode(message), hashlib.sha3_512).hexdigest()
+	else:
+		print("Invalid algorithm")
+
 
 def send_message(client, key):
 	while True:
+		alg_cript = input("\nEnter algorithm to use (1: SHA-256 - 2: SHA-512 - 3: SHA3-256 - 4: SHA3-512): ")
 		from_account = input("\nEnter your account number: ")
 		to_account = input("\nEnter the account number you want to transfer to: ")
 		amount = input("\nEnter the amount you want to transfer: ")
 		message = from_account + ":" + to_account + ":" + amount + ":" + uuid.uuid4().hex
-		mac = hmac.new(key, str.encode(message), hashlib.sha256).hexdigest()
-		message+= ":" + mac
-		if message == 'q':
-			break
+		mac = calculate_hmac(key, message, alg_cript)
+		message+= ":" + mac + ":" + alg_cript
 		client.send(str.encode(message))
 		response = client.recv(2048)
-		client.send(str.encode(message))
-		response = client.recv(2048)
-		print("RESPUESTAAAAAAAAAAAAA"+str(response.decode()))
 		response_dec=str(response.decode()).split(": ")[1]
 		id_transfer=str(response_dec).split("-")[0]
 		mac_transfer=str(response_dec).split("-")[1].strip()
@@ -40,6 +48,7 @@ def send_message(client, key):
 
 if __name__=='__main__':
 	
+
 	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	client.connect(('127.0.0.1', 1233))
 	connection_status = client.recv(1024)
